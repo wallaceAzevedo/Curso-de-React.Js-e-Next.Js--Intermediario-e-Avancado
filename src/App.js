ae9d05b86aa46e98135ec18a00e23a09526c2c13
wallace azevedo <74993539+wallaceAzevedo@users.noreply.github.com>
2022-07-12 07:52:07 -0300

    React Hook useContext - Curso React

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
-import P from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
+import { useContext, createContext, useState } from 'react';
 import './App.css';
 
-const Post = ({ post }) => {
-  console.log('Filho renderizou');
+const globalState = {
+  title: 'O título que contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+const GlobalContext = createContext();
+
+// eslint-disable-next-line
+const Div = ({ children }) => {
   return (
-    <div key={post.id} className="post">
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
+    <>
+      <H1 />
+      <P />
+    </>
   );
 };
 
-Post.propTypes = {
-  post: P.shape({
-    id: P.number,
-    title: P.string,
-    body: P.string,
-  }),
+// eslint-disable-next-line
+const H1 = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { title, counter },
+  } = theContext;
+  return (
+    <h1>
+      {title} {counter}
+    </h1>
+  );
+};
+
+// eslint-disable-next-line
+const P = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { body, counter },
+    setContextState,
+  } = theContext;
+  return (
+    <p
+      onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}
+    >
+      {body} {counter}
+    </p>
+  );
 };
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [value, setValue] = useState('');
-
-  console.log('Pai renderizou!');
-
-  // Component did mount
-  useEffect(() => {
-    setTimeout(function () {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((r) => r.json())
-        .then((r) => setPosts(r));
-    }, 5000);
-  }, []);
+  const [contextState, setContextState] = useState(globalState);
 
   return (
-    <div className="App">
-      <p>
-        <input
-          type="search"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </p>
-      {useMemo(() => {
-        return (
-          posts.length > 0 &&
-          posts.map((post) => {
-            return <Post key={post.id} post={post} />;
-          })
-        );
-      }, [posts])}
-      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
-    </div>
+    <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <Div />
+    </GlobalContext.Provider>
   );
 }
 
